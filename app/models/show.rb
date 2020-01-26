64c68c7a7d01ef7ad318b2c42ca12707c99f8e2f
github username <github email address>
2020-01-26 14:33:01 -0800

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,8 +2,35 @@ class Show < ActiveRecord::Base
 
     # private
 
-    def highest_rating
+    def self.highest_rating
         self.maximum(:rating)
     end
+
+    def self.most_popular_show
+        self.where("rating = ?", self.highest_rating)[0]
+        # Song.where("number_of_stars > ?", 3) 0x00007ff21b348658
+        # binding.pry
+    end
+
+    def self.lowest_rating
+        self.minimum(:rating)
+    end
+
+    def self.least_popular_show
+        self.where("rating = ?", self.lowest_rating)[0]
+    end
+
+    def self.ratings_sum
+        self.sum(:rating)
+    end
+
+    def self.popular_shows
+        self.where("rating > ?", 5)
+    end
+
+    def self.shows_by_alphabetical_order
+        self.order(name: :asc)
+    end
+
 end
 
