724bf3bd6e5b34d793c8d24bc64eb79dcab105c3
Scott Hoisington <hoyzington@gmail.com>
2020-01-29 00:44:28 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Bundler.require
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
 
-DB = ActiveRecord::Base.establish_connection({
+ActiveRecord::Base.establish_connection({
   adapter: 'sqlite3',
   database: 'db/tvshows.db'
 })
